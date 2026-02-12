2815ac2906cd465ab0aeeeb2ba993ef536d46e41
joewang0430 <joewj0430@gmail.com>
2026-02-12 02:36:44 -0500

    solved the download mistake issue

--- src/components/general/ui/ProfileButton.tsx
+++ src/components/general/ui/ProfileButton.tsx
@@ -5,7 +5,6 @@ const ProfileButton = () => {
   return (
     <Link
       href="/profile"
-      download
       className="inline-flex items-center px-6 py-3 bg-wz-main-color text-wz-classic-white text-lg font-semibold rounded-full
                  font-wf-title w-1/2 hover:bg-wz-secondary-color transition-all duration-300 ease-in-out"
     >
